ca1953a13974831d2ae6ffc359be50ffc53a407f
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-05-30 20:06:09 +0300

    Create 7-binary_tree_inorder.c

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_inorder:goes through a binary
+ * tree using in-order traversal
+ * @tree: the tree to transverse
+ * @func: the printing function
+ * Return: Void
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree && func)
+	{
+		binary_tree_preorder(tree->left, func);
+		func(tree->n);
+		binary_tree_preorder(tree->right, func);
+	}
+}
