a2ebf96c1835cf01790d536e1bd3735ba958139a
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-05-30 18:54:41 +0300

    Create treeprint.c

---
+++ treeprint.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+
+/**
+ * treeprint: Print a binary tree
+ * @root: The root node
+ * level: The level to start at
+ * Return: Void
+ */
+void treeprint(binary_tree_t *root, int level)
+{
+        if (root == NULL)
+                return;
+        for (int i = 0; i < level; i++)
+                printf(i == level - 1 ? "|-" : "  ");
+        printf("%d\n", root->n);
+        treeprint(root->left, level + 1);
+        treeprint(root->right, level + 1);
+}
