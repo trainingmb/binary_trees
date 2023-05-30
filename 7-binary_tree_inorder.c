211eaf062977dddbbd61baca6b6ce7b155bfd88e
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-05-30 20:09:50 +0300

    Update 7-binary_tree_inorder.c

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -12,8 +12,8 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree && func)
 	{
-		binary_tree_preorder(tree->left, func);
+		binary_tree_inorder(tree->left, func);
 		func(tree->n);
-		binary_tree_preorder(tree->right, func);
+		binary_tree_inorder(tree->right, func);
 	}
 }
