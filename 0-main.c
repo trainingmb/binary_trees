54989177d937c5b51e460d20973c8ac5676417c9
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-05-30 18:54:37 +0300

    Update 0-main.c

--- 0-main.c
+++ 0-main.c
@@ -19,6 +19,6 @@ int main(void)
 	root->right->left = binary_tree_node(root->right, 256);
 	root->right->right = binary_tree_node(root->right, 512);
 
-	binary_tree_print(root);
+	treeprint(root);
 	return (0);
 }
