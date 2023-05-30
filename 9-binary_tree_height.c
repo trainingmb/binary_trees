9016f1a1ad85ec59756e2a1f9c2d160cf8e80b15
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-05-30 20:33:32 +0300

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,6 +14,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		r = binary_tree_height(tree->right);
 		l = binary_tree_height(tree->left);
+		if (r == 0 && l == 0)
+			return (0);
 		return ((r > l? r : l) + 1);
 	}
 	return (0);
