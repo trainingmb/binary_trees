a98ecbe26901d42921b88e010f4902224b9ffba3
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-05-30 19:40:03 +0300

    Betty fix

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -26,7 +26,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	}
 	else
 	{
-		newnode->right= parent->right;
+		newnode->right = parent->right;
 		newnode->right->parent = newnode;
 		parent->right = newnode;
 	}
