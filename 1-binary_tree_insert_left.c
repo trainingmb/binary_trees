6584c8e14010cb7e757ee2eaef31b2cb74bb8ea5
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-05-30 19:23:37 +0300

    Update 1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,11 +11,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newnode;
 
+	if (parent == NULL)
+		return (NULL);
 	newnode = malloc(sizeof(binary_tree_t));
 	if (newnode == NULL)
-	{
 		return (NULL);
-	}
 	newnode->n = value;
 	newnode->right = NULL;
 	newnode->parent = parent;
@@ -33,5 +33,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 			parent->left = newnode;
 		}
 	}
+
 	return (newnode);
 }
