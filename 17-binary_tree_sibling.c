6df16e6c0b49c75e06378ac61da2eaeb39511624
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-05-30 23:28:20 +0300

    Update 17-binary_tree_sibling.c

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -10,7 +10,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (!node || !node->parent)
 		return (NULL);
 	if (node->parent->left == node)
-		return (node->parent->right)
+		return (node->parent->right);
 	else
-		return (node->parent->left)
+		return (node->parent->left);
 }
