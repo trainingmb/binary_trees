44973d7adbe953c0dd4d2fe24c92e23466faac6c
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-05-30 18:37:29 +0300

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,7 +13,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	newnode = malloc(sizeof(binary_tree_t));
 	if (newnode == NULL)
 	{
-		printf("Was Null");
 		return (NULL);
 	}
 	if (parent == NULL)
