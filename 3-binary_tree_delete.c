ed410d3dca02c759ccd66bd915f54d263e7baf28
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-05-30 19:38:56 +0300

    3. Delete

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_delete: deletes an entire
+ * binary tree
+ * @tree: the tree to delete
+ * Return: Void
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree)
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+		free(tree);
+	}
+}
--- binary_trees.h
+++ binary_trees.h
@@ -33,5 +33,6 @@ void treeprint(binary_tree_t *, int);
 binary_tree_t *binary_tree_node(binary_tree_t *, int);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *, int);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_delete(binary_tree_t *tree);
 
 #endif
