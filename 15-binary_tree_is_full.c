89d840e610b2cc5e54b8108534fb416eed72f157
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-05-30 21:35:20 +0300

    Update 15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -15,5 +15,5 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (!tree->left && !tree->right)
 		return (1);
 	return (binary_tree_is_full(tree->left) &&
-			binary_tree_is_full(tree->right))
+			binary_tree_is_full(tree->right));
 }
