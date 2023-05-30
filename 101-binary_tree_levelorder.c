c9c7ac27252c8df5d77ae08c4a14273336b7a775
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-05-31 01:58:14 +0300

    Update 101-binary_tree_levelorder.c

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -46,7 +46,7 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 	size_t height, iter;
 
 	if (!tree || !func)
-		return
+		return;
 	height = bth(tree);
 	for (iter = 0; iter < height; ++iter)
 	{
