b137e1917c81e48bda0af7a6af55d9089d3cbfbd
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-05-31 01:56:41 +0300

    Update 100-binary_trees_ancestor.c

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -7,7 +7,8 @@
  * @second: a pointer to the second node
  * Return: POinter to ancestor otherwise NULL
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+									const binary_tree_t *second)
 {
 	const binary_tree_t *ph1, *ph2;
 
