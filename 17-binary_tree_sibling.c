efa929c1dd1e04dc453c121ea3dec16398434e2a
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-05-30 23:27:02 +0300

    Create 17-binary_tree_sibling.c

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - finds the sibling of a node
+ * @node: Node for whom the sibling we seek
+ * Return: Pointer to sibling or NULL
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+	if (node->parent->left == node)
+		return (node->parent->right)
+	else
+		return (node->parent->left)
+}
